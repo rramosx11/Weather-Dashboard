7cdd70679e9d1c05f7afb861271051ade7956ec2
rramosx11 <rodolforamosd11@gmail.com>
2022-07-13 23:27:18 -0400

    displayed the 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,11 +42,19 @@ var getWeather = function (city) {
 
 var displayWeather = function (weatherData) {
   // format and display the values
-  $("#main-city-name").text(
-    weatherData.name + " " + moment().format("(MMMM/DD/YYYY)")
-  );
+  $("#main-city-name")
+    .text(
+      weatherData.name +
+        " (" +
+        dayjs(weatherData.dt * 1000).format("MM/DD/YYYY") +
+        ") "
+    )
+    .append(
+      `<img src="https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"></img>`
+    );
+  // weatherData.name + " " + moment().format("(MMMM/DD/YYYY)"));
   $("#main-city-temp").text("Temperature: " + weatherData.main.temp + " °F");
-  $("#main-city-wind").text("Wind Speed: " + weatherData.wind.speed + " mph");
+  $("#main-city-wind").text("Wind Speed: " + weatherData.wind.speed + " MPH");
   $("#main-city-humid").text("Humidity: " + weatherData.main.humidity + " %");
 
   // use lat and lon to make uvi call
@@ -80,13 +88,29 @@ var displayWeather = function (weatherData) {
       weatherData.name +
       "&appid=bda95baec6bc5b56e1a5b8d7e5c95181&units=imperial"
   ).then(function (response) {
+    console.log(response);
     response.json().then(function (data) {
+      console.log(data);
       // clear any previous entries in the five-day forecast
       $("#five-day").empty();
 
       // get every 8th value (24hours) in the returned array from the api call
       for (i = 7; i <= data.list.length; i += 8) {
         // insert data into my day forecast card template
+        var fiveDayCard =
+          "<div class='col-md-2 m-2 py-3 card text-white bg-primary'><div class='card-body p-1'><h5 class='card-title'>" +
+          dayjs(data.list[i].dt * 1000).format("MMMM/DD/YYYY") +
+          "</h5> <img src = 'https://openweathermap.org/img/wn/" +
+          data.list[i].weather[0].icon +
+          ".png' alt =''><p class='card-text'>Temperature: " +
+          data.list[i].main.temp +
+          "</p><p class='card-text'>Wind: " +
+          data.list[i].wind.speed +
+          "</p><p class='card-text'>Humidity: " +
+          data.list[i].main.humidity +
+          "</p></div></div>";
+        // append to five day forecast
+        $("#five-day").append(fiveDayCard);
       }
     });
   });
